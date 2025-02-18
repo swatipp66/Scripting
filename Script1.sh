ac8ca32ac829dcf588c68fd5f141dd3a99c9a4c1
swatipp66 <swatipp881@gmail.com>
2025-02-18 23:31:55 +0530

    this is commit in branch swati

--- Script1.sh
+++ Script1.sh
@@ -2,4 +2,5 @@ echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-26 and learning DevOps"
 echo "hello,we are learning devops"
+echo "i am adding this line in swati"
 
